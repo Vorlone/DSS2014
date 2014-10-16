6c64848174cd3a655855e93147985963e4d0025b
GrillPhil <bauknecht@medialesson.de>
2014-10-16 13:34:51 +0200

    Fixed drecks git scheiss dreck

---
+++ DSS2014.Client/DSS2014.Client.Windows/View/CustomerEditBlade.xaml.cs
@@ -0,0 +1,33 @@
+﻿using DSS2014.Client.Portable.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace DSS2014.Client.View
+{
+    public sealed partial class CustomerEditBlade : Blade
+    {
+        public CustomerEditBlade()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(object parameter)
+        {
+            ((CustomerEditViewModel)this.DataContext).InitCommand.Execute(parameter);
+        }
+    }
+}
---
+++ DSS2014.Client/DSS2014.Client.Windows/View/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using DSS2014.Client.Service;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DSS2014.Client.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ResourceLoader _resLoader = new ResourceLoader();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            TxbTitle.Text = _resLoader.GetString("AppTitle");
+        }
+
+        private void BladeFrame_Loaded(object sender, RoutedEventArgs e)
+        {
+            var bladeNavigationService = SimpleIoc.Default.GetInstance<INavigationService>();
+            if (bladeNavigationService != null && bladeNavigationService is BladeNavigationService)
+            {
+                ((BladeNavigationService)bladeNavigationService).ConfigureFrame((BladeFrame)sender);
+                ((BladeFrame)sender).NavigateTo(typeof(CustomersBlade));
+            }
+        }
+    }
+}
